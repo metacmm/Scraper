659aa1ad2c9e5f915ec5a992ebf51e3af68c4df5
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2020-01-12 19:01:24 -0800

    UI structure complete.

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    link:{
+        type: String,
+        required: true
+    },
+    abstract: {
+        type: String,
+        required: false
+    },
+    favorite:{
+        type: Boolean,
+        default:false,
+        required: true
+    },
+    site:{
+        type: Schema.Types.ObjectId
+    },
+    notes:[{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
---
+++ models/Site.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var SiteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    link:{
+        type: String,
+        required:true
+    }
+});
+
+var Site = mongoose.model("Site", NoteSchema);
+
+module.exports = Site;
